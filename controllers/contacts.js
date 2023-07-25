a857ca75a3a8b9322f80c73825f7e9a0aea51efa
oleksiiKAV <oleksii.kav@gmail.com>
2023-07-25 14:34:33 +0300

    MongoDB+favorite field

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,47 +1,84 @@
-const { listContacts, getContactById, addContactToList, updateContactById, removeContactById } = require('../models/contacts.js');
-const HttpError = require('../utils/http-error');
-const controllerWrap = require("../utils/controller-wrap")
+const HttpError = require("../utils/http-error");
+const controllerWrap = require("../utils/controller-wrap");
+const Contact = require("../models/contacts");
 
 const getContactsList = async (req, res) => {
-    res.json(await listContacts());
+    try {
+        res.json(await Contact.find());
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
 }
 
 const getContact = async (req, res) => {
-    const {contactId} = req.params;
-    const result = await getContactById(contactId);
-    if(!result) {
-        throw HttpError({status: 404, message: "Not found"});
+    try {
+        const { contactId } = req.params;
+        const result = await Contact.findById(contactId);
+        if (!result) {
+            throw HttpError({ status: 404, message: "Not found" });
+        }
+        res.json(result);
+    } catch (error) {
+        res.status(error.status || 500).json({ message: error.message || "Server error" });
     }
-    res.json(result);
 }
 
 const addContact = async (req, res) => {
-    const result = await addContactToList(req.body);
-
-    res.status(201).json(result);
+    try {
+        const result = await Contact.create(req.body);
+        res.status(201).json(result);
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
 }
 
 const deleteContact = async (req, res) => {
-    const {contactId} = req.params;
-    const result = await removeContactById(contactId);
-    if (!result) {
-      throw HttpError({status: 404, message: "Not found"});
+    try {
+        const { contactId } = req.params;
+        const result = await Contact.findByIdAndRemove(contactId);
+        if (!result) {
+            throw HttpError({ status: 404, message: "Not found" });
+        }
+        res.json({ message: "contact deleted" });
+    } catch (error) {
+        res.status(error.status || 500).json({ message: error.message || "Server error" });
     }
-    res.json({ message: 'contact deleted' });
 }
 
 const updateContact = async (req, res) => {
-    const {contactId} = req.params;
-    const result = await updateContactById(contactId, req.body);
-    if (!result) {
-      throw HttpError({status: 404, message: "Not found"});
+    try {
+        const { contactId } = req.params;
+        const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
+        if (!result) {
+            throw HttpError({ status: 404, message: "Not found" });
+        }
+        res.json(result);
+    } catch (error) {
+        res.status(error.status || 500).json({ message: error.message || "Server error" });
     }
-    res.json(result);
 }
 
+const updateFavorite = async (req, res) => {
+    try {
+        if (!Object.keys(req.body).length) {
+            throw HttpError({ status: 400, message: "missing field favorite" });
+        }
+        const { contactId } = req.params;
+        const result = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
+        if (!result) {
+            throw HttpError({ status: 404, message: "Not found" });
+        }
+        res.json(result);
+    } catch (error) {
+        res.status(error.status || 500).json({ message: error.message || "Server error" });
+    }
+};
+
 module.exports = {
-    getContactsList: controllerWrap(getContactsList), 
-    getContact: controllerWrap(getContact), 
-    addContact: controllerWrap(addContact), 
-    updateContact: controllerWrap(updateContact), 
-    deleteContact: controllerWrap(deleteContact)};
+    getContactsList: controllerWrap(getContactsList),
+    getContact: controllerWrap(getContact),
+    addContact: controllerWrap(addContact),
+    updateContact: controllerWrap(updateContact),
+    deleteContact: controllerWrap(deleteContact),
+    updateFavorite: controllerWrap(updateFavorite),
+};
